a2765b7ef4b0ad165c0b373a3bd4940ea4c191c3
wangpengfei <qq452608069@sina.com>
2017-10-09 13:46:22 +0800

    2

---
+++ app/src/main/java/com/example/a71033/nct_v1/adapter/BaseAdapter.java
@@ -0,0 +1,8 @@
+package com.example.a71033.nct_v1.adapter;
+
+/**
+ * Created by 71033 on 2017/10/9.
+ */
+
+public class BaseAdapter {
+}
---
+++ app/src/main/java/com/example/a71033/nct_v1/cache/CacheManager.java
@@ -0,0 +1,8 @@
+package com.example.a71033.nct_v1.cache;
+
+/**
+ * Created by 71033 on 2017/10/9.
+ */
+
+public class CacheManager {
+}
