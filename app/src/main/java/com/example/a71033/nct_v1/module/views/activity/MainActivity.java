79aa254033ae43efe008b98de64b41c00ad0b5f7
yudongchen <15764240573@163.com>
2017-10-11 22:18:15 +0800

    首页UI搭建

--- app/src/main/java/com/example/a71033/nct_v1/module/views/activity/MainActivity.java
+++ app/src/main/java/com/example/a71033/nct_v1/module/views/activity/MainActivity.java
@@ -8,14 +8,11 @@ import android.widget.TextView;
 import com.example.a71033.nct_v1.R;
 import com.example.a71033.nct_v1.common.BaseActivity;
 
-import butterknife.Bind;
+import butterknife.BindView;
 
 
 public class MainActivity extends BaseActivity {
 
-
-    @Bind(R.id.tv_hello)
-    TextView tvHello;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,5 +39,18 @@ public class MainActivity extends BaseActivity {
     @Override
     public void widgetClick(View v) {
 
+    }
+
+    @Override
+    public void onClick(View v) {
+        super.onClick(v);
+        switch (v.getId()) {
+            case R.id.cv_look_round:
+//              淘一淘  startActivity();
+                break;
+            case R.id.cv_time_limit:
+//              限时淘  startActivity();
+                break;
+        }
     }
 }
